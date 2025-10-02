88bdd8daffd8de2f31931707ea453b01d2ab7e6c
Barahten <41262764+Barahten@users.noreply.github.com>
2025-10-02 17:48:46 +0300

    feat: add Types

---
+++ src/types/account.ts
@@ -0,0 +1,12 @@
+export type AccountType = 'LDAP' | 'Local';
+
+export interface LabelItem { text: string }
+
+export interface Account {
+    id: string
+    labelRaw: string
+    labels: LabelItem[]
+    type: AccountType
+    login: string
+    password: string | null
+}
