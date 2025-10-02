1f3a9d1bf29f3854f9862259771adee9f772e114
Barahten <41262764+Barahten@users.noreply.github.com>
2025-10-02 18:17:34 +0300

    feat: install and add TailwindCSS

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 
+import './assets/css/style.css'  // <- импорт Tailwind
+
 
 createApp(App).use(createPinia()).mount('#app')
