177f61d54246da2330c06888b5093714a0651669
Barahten <41262764+Barahten@users.noreply.github.com>
2025-10-02 19:15:01 +0300

    feat: update app title

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,6 @@ import App from './App.vue'
 
 import './assets/css/style.css'  // <- импорт Tailwind
 
+document.title = 'Управление учетными записями'
 
 createApp(App).use(createPinia()).mount('#app')
